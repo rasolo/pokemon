b02a8fb0e45f65d5261cc340d79a3edb697554d0
Rasmus Olofsson <rasmus@bazooka.se>
2019-04-08 16:46:12 +0200

    Use EF Core 2.1+ Value Conversion to get around storing collection of primitive types

--- src/Pokemon.Core/Entities/Pokemon.cs
+++ src/Pokemon.Core/Entities/Pokemon.cs
@@ -15,31 +15,7 @@ namespace Pokemon.Core.Entities
         [Required]
         public string Name { get; set; }
         public string ImageUrl { get; set; }
-        [NotMapped]
         public List<string> Types { get; set; }
-        /// <summary> <see cref="MetaData"/> for database persistence. </summary>
-        [Obsolete("Only for Persistence by EntityFramework")]
-        public string MetaDataJsonForDb
-        {
-            get
-            {
-                return Types == null || !Types.Any()
-                           ? null
-                           : JsonConvert.SerializeObject(Types);
-            }
-
-            set
-            {
-                if(Types == null)
-                {
-                    return;
-                }
-                if (string.IsNullOrWhiteSpace(value))
-                    Types.Clear();
-                else
-                    Types = JsonConvert.DeserializeObject<List<string>>(value);
-            }
-        }
         public ICollection<Evolution> Evolutions { get; set; }
         public ICollection<Move> Moves { get; set; }
     }
--- src/Pokemon.Infrastructure/Data/AppDbContext.cs
+++ src/Pokemon.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
 namespace Pokemon.Infrastructure.Data
 {
     public class AppDbContext : DbContext
@@ -15,6 +18,12 @@ namespace Pokemon.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //EF Core 2.1+ Value conversion since EF can not store list of primitive types: https://docs.microsoft.com/en-us/ef/core/modeling/value-conversions
+            modelBuilder.Entity<Core.Entities.Pokemon>()
+            .Property(e => e.Types)
+            .HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList());
             base.OnModelCreating(modelBuilder);
         }
     }
